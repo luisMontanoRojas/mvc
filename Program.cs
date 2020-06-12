37dc3f3708ca944680d759d551b034424676b402
luisMontanoRojas <lushio142356@gmail.com>
2020-06-11 21:57:30 -0400

    some changes

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace mvc
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
